93ff4e7050f11a6db9048b4da596bfda13c25ba2
ohpian <1569898677@qq.com>
2021-09-15 12:59:32 -0500

    add an imageview

--- app/src/main/java/com/example/hello_world/MainActivity.java
+++ app/src/main/java/com/example/hello_world/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.hello_world;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imgview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
